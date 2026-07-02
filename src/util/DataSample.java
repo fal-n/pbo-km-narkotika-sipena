68f608ff367e67e456e71a46ac321aa2093048b3
Fal-N <ahmadnaufaln01@gmail.com>
2026-07-02 11:11:30 +0700

    feat: add DataSample class for sample data at first run

---
+++ src/util/DataSample.java
@@ -0,0 +1,60 @@
+package util;
+
+import model.KnowledgeRepository;
+import model.Putusan;
+
+public class DataSample {
+    public static void loadData(KnowledgeRepository repo) {
+        repo.simpan(new Putusan("1/Pid.Sus/2024/Sby", "Surabaya", "05 Jan 2024", "Ahmad Fauzi", 28, "Sabu-sabu", 5.23, "Pasal 114 ayat (2) UU No.35/2009", "Kurir", 48, 1000000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("2/Pid.Sus/2024/Sby", "Surabaya", "08 Jan 2024", "Budi Santoso", 35, "Sabu-sabu", 10.50, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 72, 2000000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("3/Pid.Sus/2024/Sby", "Surabaya", "12 Jan 2024", "Citra Dewi Lestari", 24, "Sabu-sabu", 2.10, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 18, 800000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("4/Pid.Sus/2024/Sby", "Surabaya", "15 Jan 2024", "Doni Prasetyo", 31, "Sabu-sabu", 8.70, "Pasal 114 ayat (1) UU No.35/2009", "Kurir", 36, 800000000.0, "Hj. Siti Rahayu"));
+        repo.simpan(new Putusan("5/Pid.Sus/2024/Sby", "Surabaya", "18 Jan 2024", "Eko Yulianto", 40, "Sabu-sabu", 25.30, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 120, 3000000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("6/Pid.Sus/2024/Sby", "Surabaya", "22 Jan 2024", "Fajar Hidayat", 26, "Sabu-sabu", 1.50, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 12, 600000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("7/Pid.Sus/2024/Sby", "Surabaya", "25 Jan 2024", "Gunawan Wibowo", 33, "Sabu-sabu", 15.80, "Pasal 114 ayat (2) UU No.35/2009", "Penyimpan", 60, 1500000000.0, "Hj. Siti Rahayu"));
+        repo.simpan(new Putusan("8/Pid.Sus/2024/Sby", "Surabaya", "29 Jan 2024", "Hendra Setiawan", 45, "Sabu-sabu", 30.20, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 144, 4000000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("9/Pid.Sus/2024/Sby", "Surabaya", "01 Feb 2024", "Indra Kusuma", 29, "Sabu-sabu", 3.90, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 24, 800000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("10/Pid.Sus/2024/Sby", "Surabaya", "05 Feb 2024", "Joko Triyono", 38, "Sabu-sabu", 12.40, "Pasal 114 ayat (1) UU No.35/2009", "Perantara", 48, 1200000000.0, "Hj. Siti Rahayu"));
+        repo.simpan(new Putusan("11/Pid.Sus/2024/Sby", "Surabaya", "08 Feb 2024", "Kurniawan Adi", 32, "Sabu-sabu", 7.60, "Pasal 114 ayat (1) UU No.35/2009", "Kurir", 36, 900000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("12/Pid.Sus/2024/Sby", "Surabaya", "12 Feb 2024", "Lutfi Rahardjo", 27, "Sabu-sabu", 0.80, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 8, 500000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("13/Pid.Sus/2024/Sby", "Surabaya", "15 Feb 2024", "Muhammad Arif", 42, "Sabu-sabu", 45.00, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 180, 6000000000.0, "Hj. Siti Rahayu"));
+        repo.simpan(new Putusan("14/Pid.Sus/2024/Sby", "Surabaya", "19 Feb 2024", "Nanang Santoso", 36, "Sabu-sabu", 6.30, "Pasal 114 ayat (1) UU No.35/2009", "Penyimpan", 36, 800000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("15/Pid.Sus/2024/Sby", "Surabaya", "22 Feb 2024", "Ony Kristianto", 23, "Sabu-sabu", 2.70, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 18, 700000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("16/Pid.Sus/2024/Sby", "Surabaya", "26 Feb 2024", "Priyanto Hadi", 48, "Sabu-sabu", 20.50, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 108, 2500000000.0, "Hj. Siti Rahayu"));
+        repo.simpan(new Putusan("17/Pid.Sus/2024/Sby", "Surabaya", "01 Mar 2024", "Qomaruddin Efendi", 30, "Sabu-sabu", 4.10, "Pasal 112 ayat (2) UU No.35/2009", "Penyimpan", 24, 800000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("18/Pid.Sus/2024/Sby", "Surabaya", "05 Mar 2024", "Rahmat Hidayat", 25, "Sabu-sabu", 1.20, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 9, 500000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("19/Pid.Sus/2024/Sby", "Surabaya", "08 Mar 2024", "Slamet Riyadi", 37, "Sabu-sabu", 9.80, "Pasal 114 ayat (1) UU No.35/2009", "Kurir", 48, 1000000000.0, "Hj. Siti Rahayu"));
+        repo.simpan(new Putusan("20/Pid.Sus/2024/Sby", "Surabaya", "12 Mar 2024", "Teguh Prasetyo", 43, "Sabu-sabu", 35.50, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 156, 5000000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("21/Pid.Sus/2024/Jkt.Bar", "Jakarta Barat", "15 Mar 2024", "Usman Harun", 31, "Sabu-sabu", 8.20, "Pasal 114 ayat (1) UU No.35/2009", "Kurir", 42, 900000000.0, "Dr. Wahyu Setiawan"));
+        repo.simpan(new Putusan("22/Pid.Sus/2024/Jkt.Bar", "Jakarta Barat", "19 Mar 2024", "Vina Kurniawati", 28, "Sabu-sabu", 3.50, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 15, 600000000.0, "Dr. Wahyu Setiawan"));
+        repo.simpan(new Putusan("23/Pid.Sus/2024/Jkt.Bar", "Jakarta Barat", "22 Mar 2024", "Wahyu Nugraha", 39, "Sabu-sabu", 18.70, "Pasal 114 ayat (2) UU No.35/2009", "Penyimpan", 72, 1800000000.0, "H. Rudi Santoso"));
+        repo.simpan(new Putusan("24/Pid.Sus/2024/Jkt.Bar", "Jakarta Barat", "26 Mar 2024", "Xaverius Tanto", 45, "Sabu-sabu", 52.00, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 144, 4000000000.0, "Dr. Wahyu Setiawan"));
+        repo.simpan(new Putusan("25/Pid.Sus/2024/Jkt.Bar", "Jakarta Barat", "29 Mar 2024", "Yusuf Mandala", 26, "Sabu-sabu", 2.30, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 12, 500000000.0, "H. Rudi Santoso"));
+        repo.simpan(new Putusan("26/Pid.Sus/2024/Mdn", "Medan", "02 Apr 2024", "Zainudin Parinduri", 34, "Sabu-sabu", 11.50, "Pasal 114 ayat (2) UU No.35/2009", "Kurir", 54, 1200000000.0, "Hj. Nurhayati Siregar"));
+        repo.simpan(new Putusan("27/Pid.Sus/2024/Mdn", "Medan", "05 Apr 2024", "Andika Situmorang", 29, "Sabu-sabu", 6.80, "Pasal 114 ayat (1) UU No.35/2009", "Perantara", 36, 800000000.0, "Dr. Jhon Napitupulu"));
+        repo.simpan(new Putusan("28/Pid.Sus/2024/Mdn", "Medan", "09 Apr 2024", "Bonar Siahaan", 41, "Sabu-sabu", 28.40, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 120, 3000000000.0, "Hj. Nurhayati Siregar"));
+        repo.simpan(new Putusan("29/Pid.Sus/2024/Mdn", "Medan", "12 Apr 2024", "Christin Manurung", 33, "Sabu-sabu", 4.60, "Pasal 112 ayat (1) UU No.35/2009", "Pengguna", 20, 700000000.0, "Dr. Jhon Napitupulu"));
+        repo.simpan(new Putusan("30/Pid.Sus/2024/Mdn", "Medan", "16 Apr 2024", "Dedi Hutabarat", 38, "Sabu-sabu", 15.30, "Pasal 114 ayat (2) UU No.35/2009", "Penyimpan", 60, 1500000000.0, "Hj. Nurhayati Siregar"));
+        repo.simpan(new Putusan("31/Pid.Sus/2024/Sby", "Surabaya", "19 Apr 2024", "Eko Firmansyah", 21, "Ganja", 200.50, "Pasal 111 ayat (2) UU No.35/2009", "Pengguna", 6, 500000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("32/Pid.Sus/2024/Sby", "Surabaya", "22 Apr 2024", "Firdaus Khalik", 27, "Ganja", 500.00, "Pasal 111 ayat (2) UU No.35/2009", "Bandar", 96, 2000000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("33/Pid.Sus/2024/Sby", "Surabaya", "26 Apr 2024", "Galih Saputra", 23, "Ganja", 100.20, "Pasal 111 ayat (1) UU No.35/2009", "Pengguna", 8, 500000000.0, "Hj. Siti Rahayu"));
+        repo.simpan(new Putusan("34/Pid.Sus/2024/Sby", "Surabaya", "30 Apr 2024", "Hasyim Asyari", 35, "Ganja", 750.80, "Pasal 111 ayat (2) UU No.35/2009", "Kurir", 72, 1500000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("35/Pid.Sus/2024/Sby", "Surabaya", "03 Mei 2024", "Ivan Pradhana", 29, "Ganja", 300.00, "Pasal 111 ayat (2) UU No.35/2009", "Penyimpan", 48, 1000000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("36/Pid.Sus/2024/Bdg", "Bandung", "07 Mei 2024", "Jarot Purnama", 32, "Ganja", 450.50, "Pasal 111 ayat (2) UU No.35/2009", "Bandar", 84, 1800000000.0, "H. Asep Koswara"));
+        repo.simpan(new Putusan("37/Pid.Sus/2024/Bdg", "Bandung", "10 Mei 2024", "Kiki Hendrayana", 24, "Ganja", 150.30, "Pasal 111 ayat (1) UU No.35/2009", "Pengguna", 10, 500000000.0, "Hj. Dewi Ratnasari"));
+        repo.simpan(new Putusan("38/Pid.Sus/2024/Bdg", "Bandung", "14 Mei 2024", "Lutfia Nuraeni", 19, "Ganja", 50.70, "Pasal 111 ayat (1) UU No.35/2009", "Pengguna", 6, 500000000.0, "H. Asep Koswara"));
+        repo.simpan(new Putusan("39/Pid.Sus/2024/Bdg", "Bandung", "17 Mei 2024", "Maman Suherman", 44, "Ganja", 1000.00, "Pasal 111 ayat (2) UU No.35/2009", "Bandar", 120, 3000000000.0, "Hj. Dewi Ratnasari"));
+        repo.simpan(new Putusan("40/Pid.Sus/2024/Bdg", "Bandung", "21 Mei 2024", "Neni Wahyuningsih", 30, "Ganja", 200.00, "Pasal 111 ayat (2) UU No.35/2009", "Kurir", 36, 800000000.0, "H. Asep Koswara"));
+        repo.simpan(new Putusan("41/Pid.Sus/2024/Sby", "Surabaya", "24 Mei 2024", "Ocky Aryanto", 25, "Ekstasi", 50.00, "Pasal 114 ayat (2) UU No.35/2009", "Perantara", 60, 1500000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("42/Pid.Sus/2024/Sby", "Surabaya", "28 Mei 2024", "Panji Wirawan", 33, "Ekstasi", 200.00, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 96, 2500000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("43/Pid.Sus/2024/Sby", "Surabaya", "31 Mei 2024", "Rendra Kusuma", 22, "Ekstasi", 15.50, "Pasal 113 ayat (1) UU No.35/2009", "Pengguna", 12, 600000000.0, "Hj. Siti Rahayu"));
+        repo.simpan(new Putusan("44/Pid.Sus/2024/Sby", "Surabaya", "04 Jun 2024", "Supriyadi Hartono", 40, "Ekstasi", 300.00, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 132, 4000000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("45/Pid.Sus/2024/Sby", "Surabaya", "07 Jun 2024", "Tika Rahmawati", 27, "Ekstasi", 25.00, "Pasal 113 ayat (2) UU No.35/2009", "Perantara", 30, 800000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("46/Pid.Sus/2024/Smg", "Semarang", "11 Jun 2024", "Untung Suryadi", 36, "Ekstasi", 100.00, "Pasal 114 ayat (1) UU No.35/2009", "Kurir", 48, 1000000000.0, "H. Wibowo Nugroho"));
+        repo.simpan(new Putusan("47/Pid.Sus/2024/Smg", "Semarang", "14 Jun 2024", "Vera Anggraini", 29, "Ekstasi", 50.50, "Pasal 113 ayat (2) UU No.35/2009", "Penyimpan", 24, 800000000.0, "Hj. Siwi Prihantini"));
+        repo.simpan(new Putusan("48/Pid.Sus/2024/Sby", "Surabaya", "18 Jun 2024", "Wahyudi Santoso", 45, "Heroin", 30.00, "Pasal 114 ayat (2) UU No.35/2009", "Bandar", 168, 7000000000.0, "H. Bambang Sutrisno"));
+        repo.simpan(new Putusan("49/Pid.Sus/2024/Sby", "Surabaya", "21 Jun 2024", "Xenia Puspitasari", 31, "Heroin", 10.50, "Pasal 112 ayat (2) UU No.35/2009", "Penyimpan", 72, 2000000000.0, "Drs. Agus Salim"));
+        repo.simpan(new Putusan("50/Pid.Sus/2024/Sby", "Surabaya", "25 Jun 2024", "Yanuar Firmansyah", 28, "Heroin", 5.20, "Pasal 114 ayat (1) UU No.35/2009", "Kurir", 54, 1500000000.0, "Hj. Siti Rahayu"));
+        System.out.println("  [INFO] " + repo.getTotalData() + " data sampel berhasil dimuat ke dalam sistem.");
+    }
+}
